b96f41e809f1b800ada009d4a91d78a9178d9113
jayce <tkzj_happy@sina.com>
2024-05-10 20:07:05 +0800

    Write Duty

---
+++ src/main/java/model/bean/Duty.java
@@ -0,0 +1,18 @@
+package model.bean;
+
+/**
+ * 功能注释：
+ *
+ * @author Jayce
+ * @create 2024-05-10
+ */
+public class Duty {
+
+    private Integer id;
+
+    private String name;
+
+    public void test(){
+        System.out.println(this.id);
+    }
+}
